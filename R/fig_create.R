1de3af1407e3ec6871b6700ae79b3d32ed9a8e2d
Ian Handel <ian.handel@ed.ac.uk>
2018-02-11 16:31:15 -0400

    try and stop CRAN note on global variables

--- R/fig_create.R
+++ R/fig_create.R
@@ -30,8 +30,13 @@ fig_create <- function(filenames = NULL,
   if (!is.null(dir)) {
     filenames <- fs::dir_ls(dir)
   }
+
+  ## quiets concerns of R CMD check re: the .'s that appear in pipelines
+  if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
+
   filenames <- filenames[stringr::str_detect(filenames, extensions)]
   filenames_short <- fs::path_file(filenames)
+#
   tbl <- tibble::tibble(filenames, filenames_short) %>%
     dplyr::mutate(index = seq_along(filenames),
                   link = glue::glue_data(.,
