452b06e585c521f49aa7602a565d6093726526fb
Ian Handel <ian.handel@ed.ac.uk>
2018-02-11 16:31:36 -0400

    starts fig_write

---
+++ R/fig_write.R
@@ -0,0 +1,19 @@
+#' Write a file including list of links, figures and labels
+#'
+#' This function is given a data.frame of filenames, links and labels. It writes out
+#' a mardkdown, pdf or html file includign the figures, labels and a table
+#'  of links
+#'
+#' @param tbl A data.frame with full paths, links and labels
+#' @param filename A path to a directory of image files
+#' @return Returns tbl, side effect is to write output.
+#'
+#' @examples
+#' \dontrun{
+#' fig_write(tbl = mydata, filename = "myfilename.md")
+#' }
+#'
+
+fig_write <- function(tbl, filename){
+
+}
