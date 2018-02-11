4f42fc68fe857371cbe541dcfdd58202bafa90ca
Ian Handel <ian.handel@ed.ac.uk>
2018-02-11 16:58:05 -0400

    adds markdown header

--- R/fig_write.R
+++ R/fig_write.R
@@ -1,11 +1,13 @@
 #' Write a file including list of links, figures and labels
 #'
 #' This function is given a data.frame of filenames, links and labels. It writes out
-#' a mardkdown, pdf or html file includign the figures, labels and a table
+#' a mardkdown, pdf or html file including the figures, labels and a table
 #'  of links
 #'
 #' @param tbl A data.frame with full paths, links and labels
 #' @param filename A path to a directory of image files
+#' @param author Author text for output file
+#' @param date Date text for output file
 #' @return Returns tbl, side effect is to write output.
 #'
 #' @examples
@@ -14,6 +16,21 @@
 #' }
 #'
 
-fig_write <- function(tbl, filename){
+fig_write <- function(tbl, filename,
+                      author = NULL,
+                      date = NULL){
+
+  # markdown file headers
+  fs::file_create(filename)
+  readr::write_lines(path = filename, "---", append = TRUE)
+  if (!is.null(author)){
+    readr::write_lines(path = filename, "author: \"{author}\"", append = TRUE)
+  }
+  if (!is.null(date)){
+    readr::write_lines(path = filename, "date: \"{date}\"", append = TRUE)
+  }
+  readr::write_lines(path = filename, "output: md_document", append = TRUE)
+  readr::write_lines(path = filename, "---", append = TRUE)
 
 }
+
