be237b2aae6f6023567fbb285f61ecef6ea20859
Ian Handel <ian.handel@ed.ac.uk>
2018-02-13 05:40:51 -0400

    adds fig_label

---
+++ R/fig_homelabel.R
@@ -0,0 +1,20 @@
+#' Create a named label - defaults to 'home'
+#'
+#' This function creates an html label tag manually, defaulting
+#' to 'home'. Likely to be used to give a return location from
+#' figures,
+#'
+#' @param label
+#' @return Returns nothing, sideeffect is printing label
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' fig_label()
+#' }
+#'
+
+
+fig_label <- function(label = "home"){
+  cat(glue::glue('<a id=\"{label}\"></a>'))
+}
