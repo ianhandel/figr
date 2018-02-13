79f4d887752d7931e8353a7766699128bb6419fb
Ian Handel <ian.handel@ed.ac.uk>
2018-02-13 06:29:46 -0400

    adds fig_table to write out table!

---
+++ R/fig_table.R
@@ -0,0 +1,26 @@
+#' Writes out a figr table
+#'
+#' This fucntion will insert a figr table dropping the long filenames
+#' unless these are specifically requested
+#'
+#' @param fig_tbl A figr_table
+#' @param show_all If TRUE long filename output too
+#' @return Returns fig_tbl, sideeffect is to print it
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' fig_table(tbl)
+#' }
+#'
+
+fig_table <- function(fig_tbl, show_all = FALSE){
+  if (show_all){
+    knitr::kable(fig_tbl)
+  }else{
+    knitr::kable(dplyr::select(dplyr::rename(fig_tbl,
+                                             file = filenames_short),
+                               -filenames))
+  }
+}
+
